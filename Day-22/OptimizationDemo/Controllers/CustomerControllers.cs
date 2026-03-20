1ab65e5cb9f1b619b754e02bfe1e547ca1639ccf
Tejashri-Bhoyar <tejashribhoyar11@gmail.com>
2026-03-20 17:34:08 +0530

    Day 22 - Controller Optimization and Day 18 Connection Pool

---
+++ Day-22/OptimizationDemo/Controllers/CustomerControllers.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class CustomerController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        // Database
+        var customers = await dbContext.Customers.ToListAsync();
+
+        return await Task.FromResult(Ok(customers));
+    }
+}
+
